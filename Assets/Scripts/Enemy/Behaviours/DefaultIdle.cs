55fdc47e99ba1f64e87748559b285f8d39b27436
Will Hart <clouduniversemc@gmail.com>
2020-07-20 12:01:54 +1000

    Recommit with .gitignore
    Move all gitignore folders & files into root unity file.
    (e.g. .gitignore should be chilling with Assets and Library folders)

---
+++ Assets/Scripts/Enemy/Behaviours/DefaultIdle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultIdle : AIBehaviour
+{
+    [Header("Properties")]
+    public float initialDetectionDistance = 15.0f;
+
+    public override void OnEnter() { }
+
+    public override void OnExit() { }
+
+    public override void OnFixedUpdate() { }
+
+    public override void OnUpdate()
+    {
+        if (brain.GetDistanceToPlayer() < initialDetectionDistance)
+            brain.SetBehaviour("Movement");
+    }
+}
---
+++ Assets/Scripts/Enemy/Behaviours/Minion/MinionMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionMovement : AIBehaviour
+{
+    [Header("General Movement")]
+    public float acceleration = 50.0f;
+    public float maxVelocity = 10.0f;
+    public float turnSpeed = 0.20f;
+
+    [Header("Sprinting")]
+    public float sprintAcceleration = 75.0f;
+    public float rangeOfSprint = 7.5f;
+
+    [Header("Attack Transition / Avoid")]
+    public float avoidPushbackForce = 5.0f;
+    public float attackPushback = 2.5f;
+    public float attackDistance = 2.0f;
+
+    // Attack RNG
+    private int _attackRng = 0;
+
+    public override void OnEnter() {}
+
+    public override void OnExit() {}
+
+    public override void OnFixedUpdate() 
+    {
+        // Get direction towards player
+        Vector3 dir = brain.GetDirectionToPlayer();
+
+        // Rotate to face direction
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), turnSpeed);
+
+        // Move rigidbody forwards
+        if (rigidbody.velocity.magnitude < maxVelocity)
+            rigidbody.AddForce(transform.forward * acceleration * Time.fixedDeltaTime, ForceMode.Impulse);
+
+        // Check if close enough to enter attack state
+        if (brain.GetDistanceToPlayer() < attackDistance)
+        {
+            if(_attackRng < 1)
+            {
+                brain.SetBehaviour("Attack");
+                rigidbody.AddForce(-dir * attackPushback, ForceMode.Impulse);
+                _attackRng = Random.Range(0, 3);
+            }
+            else
+            {
+                rigidbody.AddForce(-dir * avoidPushbackForce, ForceMode.Impulse);
+                _attackRng = Random.Range(0, 3);
+            }
+        }
+    }
+
+    public override void OnUpdate() {}
+}
