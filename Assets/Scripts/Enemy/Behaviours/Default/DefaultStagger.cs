2bd7c67095ba40ee8ebbcda23588b7985cb990e8
Lachlan Wernert <43165818+wernut@users.noreply.github.com>
2020-07-22 18:30:51 +1000

    Added missing DefaultStagger.cs

---
+++ Assets/Scripts/Enemy/Behaviours/Default/DefaultStagger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultStagger : AIBehaviour
+{
+    [Header("Properties")]
+    public float staggerTime = 2.0f;
+
+    private bool _isStaggered = false;
+
+    public override void OnEnter() 
+    {
+        if(!_isStaggered)
+            StartCoroutine(StaggerSequence());
+    }
+
+    public override void OnExit(){}
+
+    public override void OnFixedUpdate() {}
+
+    public override void OnUpdate() {}
+
+    private IEnumerator StaggerSequence()
+    {
+        _isStaggered = true;
+        yield return new WaitForSecondsRealtime(staggerTime);
+        _isStaggered = false;
+        brain.SetBehaviour("Movement");
+    }
+}
