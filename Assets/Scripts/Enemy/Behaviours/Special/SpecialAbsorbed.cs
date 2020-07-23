d14e1e0c28fbe2a8148fe43ade96f4fd9c078e42
Lachlan Wernert <43165818+wernut@users.noreply.github.com>
2020-07-23 21:44:44 +1000

    Added missing AI behaviour

---
+++ Assets/Scripts/Enemy/Behaviours/Special/SpecialAbsorbed.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialAbsorbed : AIBehaviour
+{
+
+    /*
+     * 
+     *  Currently just destroying the object. Will implement the shader manipulation in here somewhere.
+     * 
+     * 
+     */
+
+    public override void OnEnter()
+    {
+        Destroy(this.gameObject);
+    }
+
+    public override void OnExit()
+    {
+    }
+
+    public override void OnFixedUpdate()
+    {
+    }
+
+    public override void OnUpdate()
+    {
+    }
+}
