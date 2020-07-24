fdfa17b57f533e6b0f29f53d40bf71b6817471dc
Lachlan Wernert <43165818+wernut@users.noreply.github.com>
2020-07-24 14:50:32 +1000

    Hooked up the hammer animation to the ability.

--- Assets/Scripts/Character/Abilities/AbilityHammer.cs
+++ Assets/Scripts/Character/Abilities/AbilityHammer.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class AbilityHammer : Ability
 {
+    [Header("References")]
+    public ParticleSystem groundSmashParticleSystem;
+    public AudioSource groundSmashAudio;
+
     [Header("Properties")]
     public float damageToMinion = 50.0f;
     public float damageToSpecial = 20.0f;
     public float damageToElite = 100.0f;
-
-    [Header("Timers")]
-    public float turnOffTime = 2.0f;
 
     [Header("Sphere Cast")]
     public float radius = 10.0f;
@@ -18,15 +19,21 @@ public class AbilityHammer : Ability
     [Header("Debug")]
     public bool drawSphereGizmo = false;
 
-    private bool _check = false;
+    private bool _hasRan = true;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = this.GetComponent<Animator>();
+    }
 
     public override void OnEnter() {}
     public override void OnExit() {}
     public override void OnFixedUpdate() 
     {
-        if (!_check)
+        if (!_hasRan)
         {
-            StartCoroutine(TurnOffSequence());
+            //StartCoroutine(TurnOffSequence());
 
             RaycastHit[] hits = Physics.SphereCastAll(transform.position, radius, Vector3.up, 0.0f);
             foreach (RaycastHit hit in hits)
@@ -52,7 +59,7 @@ public class AbilityHammer : Ability
                     hit.transform.gameObject.GetComponent<AIBrain>().TakeDamage(damage, AbilityManager.E_Ability.HAMMER);
                 }
             }
-            _check = true;
+            _hasRan = true;
         }
     }
 
@@ -60,24 +67,28 @@ public class AbilityHammer : Ability
 
     public override void Activate()
     {
-        //colliderToToggle.enabled = true;
-        //meshRendererToToggle.enabled = true;
+        animator.SetBool("HammerAttack", true);
         active = true;
     }
 
     public override void Deactivate()
     {
-        //colliderToToggle.enabled = false;
-        //meshRendererToToggle.enabled = false;
-        _check = false;
+        animator.SetBool("HammerAttack", false);
         active = false;
+        _hasRan = true;
         abilityManager.SetAbility(AbilityManager.E_Ability.NONE);
     }
 
-    private IEnumerator TurnOffSequence()
+    public void ActivateHammerCheck()
     {
-        yield return new WaitForSecondsRealtime(turnOffTime);
-        Deactivate();
+        _hasRan = false;
+        ActivateHammerGroundSmash();
+    }
+
+    public void ActivateHammerGroundSmash()
+    {
+        groundSmashParticleSystem.Play();
+        groundSmashAudio.Play();
     }
 
     private void OnDrawGizmos()
