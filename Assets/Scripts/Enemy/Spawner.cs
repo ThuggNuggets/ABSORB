0a01768c751dc00c394fc872dc58ab1f82a1b58c
Will Hart <clouduniversemc@gmail.com>
2020-07-28 11:48:32 +1000

    Added set amount to spawn on spawner script
    Messing with particle force fields

--- Assets/Scripts/Enemy/Spawner.cs
+++ Assets/Scripts/Enemy/Spawner.cs
@@ -10,36 +10,44 @@ public class Spawner : MonoBehaviour
     [Header("References")]
     // The transform the enemy will target
     public Transform playerTransform;
-
     public GameObject objectToSpawn;
     [Header("Spawner Properties")]
     public float numberOfEnemies = 1.0f;
     public int maxNumberOfSpawns = 0;
     public float addNumberOfEnemies = 0.2f;
     public float timeBetweenEachEnemySpawn = 5f;
+    [Header("Only spawn the max number of enemies")]
+    public bool setSpawnAmount = false;
     [Header("Keep off if you want to spawn first wave instantaneously")]
     public bool waitForSpawn = false;
     [Space]
     public Transform[] spawnerPositions;
-
     public Dictionary<string, List<GameObject>> gameObjectsByTag;
-    private string spawnTag;
+
+    private string _spawnTag;
     public int numberOfTaggedObjects = 0;
-    private float tempSpawnTimer = 0.0f;
+    private int _checkAmountOfSpawns = 0;
+    private float _tempSpawnTimer = 0.0f;
+
+    // TODO:
+    // - Set a bool to check if you only want a certain amount to spawn and thats it
 
     // Use this for initialization
     void Start()
     {
         gameObjectsByTag = new Dictionary<string, List<GameObject>>();
-        tempSpawnTimer = timeBetweenEachEnemySpawn;
-        spawnTag = objectToSpawn.tag;
+        _tempSpawnTimer = timeBetweenEachEnemySpawn;
+        _spawnTag = objectToSpawn.tag;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Delete))
+        if (Input.GetKeyDown(KeyCode.Delete) && GetEnemyFromTag(_spawnTag) != null)
+        {
             RemoveEnemy(objectToSpawn);
+            Debug.Log("Enemy removed from dictionary");
+        }
 
         // Only run if a objectToSpawn has been assigned
         if (objectToSpawn != null)
@@ -47,9 +55,9 @@ public class Spawner : MonoBehaviour
             // TODO:
             // - Potentially spawn more enemies if 1 dies (e.g. 1 enemy dies, 2 more spawn)
 
-            tempSpawnTimer -= Time.deltaTime;
+            _tempSpawnTimer -= Time.deltaTime;
 
-            if (tempSpawnTimer <= 0.0f)
+            if (_tempSpawnTimer <= 0.0f)
             {
                 waitForSpawn = false;
             }
@@ -58,37 +66,41 @@ public class Spawner : MonoBehaviour
             // for the time between each enemy to reach 0 seconds
             if (!waitForSpawn)
             {
-                tempSpawnTimer -= Time.deltaTime;
+                _tempSpawnTimer -= Time.deltaTime;
                 for (int i = 0; i < (int)numberOfEnemies; i++)
                 {
                     AddEnemy(objectToSpawn);
                 }
 
-                tempSpawnTimer = timeBetweenEachEnemySpawn;
+                _tempSpawnTimer = timeBetweenEachEnemySpawn;
                 numberOfEnemies += addNumberOfEnemies;
                 waitForSpawn = true;
             }
 
-            if (GetEnemyFromTag(spawnTag) != null)
-                numberOfTaggedObjects = GetEnemyFromTag(spawnTag).Count;
+            if (GetEnemyFromTag(_spawnTag) != null)
+                numberOfTaggedObjects = GetEnemyFromTag(_spawnTag).Count;
             //numberOfTaggedObjects = GameObject.FindGameObjectsWithTag(spawnTag).Length;
         }
     }
 
-    // Come back to try and get working
     public void AddEnemy(GameObject enemy)
     {
         if (!gameObjectsByTag.ContainsKey(enemy.tag))
             gameObjectsByTag.Add(enemy.tag, new List<GameObject>());
 
-        // Only run if we haven't reached the max number
-        if (gameObjectsByTag[enemy.tag].Count < maxNumberOfSpawns || maxNumberOfSpawns == 0)
+        // If a set spawn amount is set, then it cant spawn anymore than what has spawned
+        if (setSpawnAmount && _checkAmountOfSpawns != maxNumberOfSpawns || !setSpawnAmount)
         {
-            gameObjectsByTag[enemy.tag].Add(enemy);
-            int spawnNumber = Random.Range(0, spawnerPositions.Count());
-            AIBrain aIBrain = Instantiate(objectToSpawn, spawnerPositions[spawnNumber].transform.position, Quaternion.identity).GetComponent<AIBrain>();
-            aIBrain.playerTransform = playerTransform;
-            aIBrain.GetComponent<EnemyHandler>().SetupSpawner(this);
+            // Only run if we haven't reached the max number
+            if (gameObjectsByTag[enemy.tag].Count < maxNumberOfSpawns || maxNumberOfSpawns == 0)
+            {
+                gameObjectsByTag[enemy.tag].Add(enemy);
+                int spawnNumber = Random.Range(0, spawnerPositions.Count());
+                AIBrain aIBrain = Instantiate(objectToSpawn, spawnerPositions[spawnNumber].transform.position, Quaternion.identity).GetComponent<AIBrain>();
+                aIBrain.playerTransform = playerTransform;
+                aIBrain.GetComponent<EnemyHandler>().SetupSpawner(this); // Clean this up sometime
+                _checkAmountOfSpawns += 1;
+            }
         }
     }
 
