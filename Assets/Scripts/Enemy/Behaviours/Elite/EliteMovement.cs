be2a5ad8b3c01e96c182eb9e77aa6f3e6cf0269a
Lachlan Wernert <43165818+wernut@users.noreply.github.com>
2020-08-11 09:20:30 +1000

    Reverted EliteMovement.cs

--- Assets/Scripts/Enemy/Behaviours/Elite/EliteMovement.cs
+++ Assets/Scripts/Enemy/Behaviours/Elite/EliteMovement.cs
@@ -4,25 +4,12 @@ using UnityEngine;
 
 public class EliteMovement : AIBehaviour
 {
-    /*
-     *  Notes to keep in mind:
-     *  Complex movement, ranged attacks. Movement will be the key for this to feel good.
-     *  
-     * TODO LIST:
-     * 1) Slowly walks around the player at average range
-     * 2) Moves away when the player gets close to it
-     * 3) If the player gets to close, dashes around and away from the player by a small amount
-     * 4) Attack triggers between certain times
-     * 
-     */
-
     [Header("General Movement")]
     public float acceleration = 50.0f;
     public float retreatDashAcceleration = 75.0f;
     public float retreatMaxVelocity = 20.0f;
     public float maxVelocity = 10.0f;
     public float turnSpeed = 0.20f;
-    public float wallCheckDistance = 5.0f;
 
     [Header("Attack")]
     public float attackDistance = 15.0f;
@@ -37,7 +24,7 @@ public class EliteMovement : AIBehaviour
 
     public override void OnExit() {}
 
-    public override void OnFixedUpdate() 
+    public override void OnFixedUpdate()
     {
         // If the enemy is already waiting to attack, then exit this out of this function
         if (_isWaitingToAttack)
@@ -53,14 +40,11 @@ public class EliteMovement : AIBehaviour
         // Retreat from player if they get too close
         if (dist < attackDistance)
         {
-            // Gets the nearest the direction away from the nearest collider when avoiding.
-            Vector3 avoidDir = GetAvoidDirection(10.0f, 1000.0f);
-
             // Only adding force if velocity is under max velocity
             if (rigidbody.velocity.magnitude < retreatMaxVelocity)
             {
                 // Moving towards player if out of attack distance
-                rigidbody.AddForce(avoidDir * retreatDashAcceleration * Time.fixedDeltaTime, ForceMode.Impulse);
+                rigidbody.AddForce(-transform.forward * retreatDashAcceleration * Time.fixedDeltaTime, ForceMode.Impulse);
             }
         }
 
@@ -84,45 +68,5 @@ public class EliteMovement : AIBehaviour
         yield return new WaitForSecondsRealtime(beforeAttackTimer);
         brain.SetBehaviour("Attack");
         _isWaitingToAttack = false;
-    }
-
-    public Vector3 GetAvoidDirection(float radius, float distance)
-    {
-        Vector3 dir = Vector3.zero;
-        Vector3 calDir = Vector3.zero;
-        float dist = 0.0f;
-
-        // Get the direction
-        for(int i = 0; i < 3; ++i)
-        {
-            switch (i)
-            {
-                case 0:
-                    dir = brain.transform.right;
-                    break;
-
-                case 1:
-                    dir = -brain.transform.right;
-                    break;
-
-                case 2:
-                    dir = -brain.transform.forward;
-                    break;
-            }
-
-            // Check side
-            RaycastHit hit;
-            Physics.SphereCast(brain.transform.position, radius, dir, out hit, distance, LayerMask.NameToLayer("EnemyMovementRaycast"));
-
-            // Check if it is closer than our original distance
-            if (hit.distance <= dist)
-            {
-                dist = hit.distance;
-                calDir = dir;
-            }
-        }
-
-        // Returning the direction away from the closest hit
-        return calDir;
     }
 }
