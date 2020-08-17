88b7b8e47ebd4d433ba6d3d51f29d934610be5c0
Lachlan Wernert <43165818+wernut@users.noreply.github.com>
2020-08-18 09:22:09 +1000

    Added Alpha scripts

---
+++ Assets/Scripts/Alpha/Character/PlayerHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHandler : MonoBehaviour
+{
+    public int maxHealth = 10;
+    private bool isAlive = false;
+    private int currentHealth = 10;
+    private bool canShield = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool GetIsAlive()
+    {
+        return isAlive;
+    }
+
+    public int GetCurrentHealth()
+    {
+        return currentHealth;
+    }
+
+    public bool GetCanShield()
+    {
+        return canShield;
+    }
+}
