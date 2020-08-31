0780f454477a68d4893c3458fa9726ba506748aa
Will Hart <clouduniversemc@gmail.com>
2020-08-31 15:51:35 +1000

    Added Main Menu and Loading Scene

---
+++ Assets/Scripts/Alpha/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    private string loadingScreen = "Loading_Scene";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void Play()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(loadingScreen);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
