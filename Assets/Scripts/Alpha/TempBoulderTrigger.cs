dee29ce1b0d00081d3a5ccfbec40552a8275512c
Will Hart <clouduniversemc@gmail.com>
2020-09-08 13:39:28 +1000

    Door opens

--- Assets/Scripts/Alpha/TempBoulderTrigger.cs
+++ Assets/Scripts/Alpha/TempBoulderTrigger.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class TempBoulderTrigger : MonoBehaviour
 {
+    public BoxCollider boxCollider;
+    public Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        boxCollider.enabled = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.layer == LayerMask.NameToLayer("PlayerMelee"))
+        {
+            animator.SetBool("DoorTriggered", true);
+            boxCollider.enabled = false;
+        }
     }
 }
