f8bc20b0f3ea84f8b11098148336d2c8337336e5
Will Hart <clouduniversemc@gmail.com>
2020-09-14 11:57:49 +1000

    Added Death Trigger for Fog
    Also added variable for Time till you respawn

--- Assets/Scripts/Alpha/Character/Handlers/CombatHandler.cs
+++ Assets/Scripts/Alpha/Character/Handlers/CombatHandler.cs
@@ -314,6 +314,8 @@ public class CombatHandler : MonoBehaviour
     #endregion
 
     #region Death
+    [Header("Death Attributes")]
+    public float timeTillRespawn = 3.0f;
     private bool _respawning = false;
 
     private void UpdateDeath()
@@ -331,7 +333,6 @@ public class CombatHandler : MonoBehaviour
         if (_playerHandler.GetCurrentHealth() <= 0)
         {
             _playerHandler.SetIsAlive(false);
-            //Debug.Log("Player dead");
         }
 
         // Is the player is dead
@@ -344,7 +345,7 @@ public class CombatHandler : MonoBehaviour
 
     private IEnumerator Respawn()
     {
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(timeTillRespawn);
         // Go to last checkpoint position
         _playerHandler.RespawnPlayer();
         _respawning = false;
